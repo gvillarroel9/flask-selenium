e85d3f16b956e5be948f2a5edeec459cf49fcf99
unknown <gvillarroel9@gmail.com>
2019-07-01 22:34:00 -0400

    first commit

---
+++ prueba.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+import requests
+import json
+
+
+current_directory = "NONe"
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Worldaa!'
+
+@app.route('/consultar')
+def consulta(id=None):
+    params = {'company':'1', 'searchType':'nro_suministro','client':'3281167-1', 'dtmId':'account payment'}
+    response = requests.post('https://www.enel.cl/es/clientes/servicios-en-linea/pago-de-cuenta.mdwedge.getSupplyDetail.html', data = params)
+    factura=json.loads(response.text)['supplyBalance']
+    return render_template('consulta.html', monto=factura)
+
+@app.route('/lista/')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+app.run(debug=True, port=8000)
