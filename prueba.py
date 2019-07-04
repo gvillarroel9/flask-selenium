8d1e38692f1c6827680ade62251b2887014994cd
unknown <gvillarroel9@gmail.com>
2019-07-04 17:57:21 -0400

    selenium heroku

--- prueba.py
+++ prueba.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask import render_template
 import requests
+from selenium import webdriver
 from flask_cors import CORS
 
 
@@ -19,8 +20,18 @@ def consulta(client_id=None):
     response = requests.post('https://www.enel.cl/es/clientes/servicios-en-linea/pago-de-cuenta.mdwedge.getSupplyDetail.html', data = params)
     return response.content
 
-@app.route('/lista/')
+@app.route('/selenium/')
 def hello(name=None):
+    chrome_options = Options()
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver.get("https://www.eneldistribucion.cl")    
+    driver.get("http://www.python.org")
+    element = driver.find_element_by_id("success-stories")
+    name=element.text
+    driver.close()
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
