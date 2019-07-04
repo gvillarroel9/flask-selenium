b7d8c66b5f9afac85cf55534cf0c828d6598e4c4
unknown <gvillarroel9@gmail.com>
2019-07-04 18:09:20 -0400

    correccion selenium path chrome y chromedriver

--- prueba.py
+++ prueba.py
@@ -1,8 +1,21 @@
 from flask import Flask, jsonify
 from flask import render_template
 import requests
-from selenium import webdriver
 from flask_cors import CORS
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+import os
+
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
+GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
+options = Options()
+options.binary_location = GOOGLE_CHROME_BIN
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.headless = True
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
 
 current_directory = "NONe"
@@ -22,11 +35,6 @@ def consulta(client_id=None):
 
 @app.route('/selenium/')
 def hello(name=None):
-    chrome_options = Options()
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get("http://www.python.org")
     element = driver.find_element_by_id("success-stories")
     name=element.text
